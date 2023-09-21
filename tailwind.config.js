84bb958894380d84cd1ee2d276992647c702af6d
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-09-20 18:48:20 -0700

    added styles for nav search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './assets/script.js'],
+  content: ['./index.html', './assets/script.js', "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
