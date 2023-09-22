1f22122fd0c91e72c5ee01e0666b6156cb0dc578
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-09-22 11:32:41 -0700

    added code to get city name from API

--- assets/script.js
+++ assets/script.js
@@ -1,82 +1,26 @@
-// input city data fetch to display info  
-  
 
-
-
-var dispName = document.querySelector(".display-input-name")
-var dispCountry = document.querySelector(".display-input-country")
-var dispPopulation = document.querySelector(".display-input-population")
-var dispLanguage = document.querySelector(".display-input-language")
-var dispCurrency = document.querySelector(".display-input-currency")
-var dispTime = document.querySelector(".display-input-time")
-var dispAbout = document.querySelector(".display-input-about")
-var cityCardHeader = document.querySelector('#city-card')
 
 
 // Navigation/Search Bar
+
 var searchBar = document.querySelector('#search-bar');
 var searchButton = document.querySelector('#search-submit');
 
 searchBar.addEventListener('submit', function (event) {
     event.preventDefault();
-   
+    //console.log(event.target.searchTerm.value); // Accessing the city typed in the search bar
     var searchCity = event.target.searchTerm.value;
 
+    // Searching for hotels with searchTerm.value below
+    document.querySelector("#hotels").innerHTML = "";
+    getCityID(event.target.searchTerm.value);
 
-        getCityUrlShawn(searchCity);
-    
-//console.log(event.target.searchTerm.value); // Accessing the city typed in the search bar
-
-
-// Searching for hotels with searchTerm.value below
-document.querySelector("#hotels").innerHTML = "";
-getCityID(event.target.searchTerm.value);
-
-if (searchCity) {
-    cityHeader.setAttribute("class", "hidden");
-    getCityURL(searchCity);
-}
+    if (searchCity) {
+        cityHeader.setAttribute("class", "hidden");
+        getCityURL(searchCity);
+    }
 })
 
-
-
-
-
-//fetch request to grab photos, name ,country and population for searched city
-
-function showName(cityUrl,searchCity) {
-    fetch(cityUrl).then(function(response) {
-        return response.json();
-    }).then(function(data){
-        console.log(data._links['city:country'].name)
-      dispName.textContent = data.name
-    dispCountry.textContent = data._links['city:country'].name
-    dispPopulation.textContent = ('POPULATION: '+ data.population)  
-    })
-
-}
-
-
-
-function getCityUrlShawn(searchCity) {
-    var url = 'https://api.teleport.org/api/cities/?search=' + searchCity;
-    var cityUrl = [];
-   
-    fetch(url).then(function(response) {
-        return response.json();
-    }).then(function(data){ 
-        var cities = data._embedded["city:search-results"];
-
-        for (let index = 0; index < cities.length; index++) {
-            const element = cities[index];
-            cityUrl.push(element._links["city:item"].href);
-        }
-        cityUrl = cityUrl[0];
-        console.log(cityUrl)
-       urlofCity(cityUrl,searchCity)
-       showName(cityUrl, searchCity)
-    })
-}
 // City Header
 
 var cityHeader = document.querySelector('#city-header');
@@ -95,69 +39,30 @@ function getCityURL(searchCity) {
             const element = cities[index];
             cityURL.push(element._links["city:item"].href);
         }
+
         cityURL = cityURL[0];
-        getCity(cityURL, searchCity);
-       
+        //console.log(cityURL)
+        getCity(cityURL);
     })
 }
 
-function urlofCity(cityUrl, searchCity) {
-         var cityPath;
-         fetch(cityUrl).then(function(response) {
-             return response.json();
-         }).then(function (data) {
-             cityPath = data._links["city:urban_area"].href;
-    
-             console.log(cityPath);
-             getImageAbout(cityPath, searchCity)
-       })
-    }
-
-function getImageAbout(cityPath,searchCity) {
-    var imageUrlAbout;
-    fetch(cityPath).then(function(response) {
-        return response.json();
-    
-    }).then(function(data) {
-        imageUrlAbout = data._links["ua:images"].href;
-        console.log(imageUrlAbout);
-        getImageShawn(imageUrlAbout, searchCity);
-    })
-}
-
-
- function getImageShawn(imageUrlAbout,searchCity){
- fetch(imageUrlAbout).then(function(response) {
-     return response.json();
- }).then(function(data) {
-  
-    var imgCity = data.photos[0].image.web
-       console.log(imgCity)
-     setImg(imgCity,searchCity)
- })
- }
-    function setImg (imgCity,searchCity){
-
-   cityCardHeader.src = imgCity
-  dispName.innerHTML = searchCity;
-    }
-       
-
-function getCity(cityurl, searchCity) {
+function getCity(cityurl) {
     var cityIdURL;
+    var cityNameAPI;
     fetch(cityurl).then(function(response) {
         return response.json();
     }).then(function (data) {
         cityIdURL = data._links["city:urban_area"].href;
-        // cityImage = data.photos[0].image.web;
+        cityNameAPI = data.full_name;
 
         //console.log(cityIdURL);
-        getCityImageURL(cityIdURL, searchCity)
+        //console.log(cityNameAPI)
+        getCityImageURL(cityIdURL, cityNameAPI)
     })
     
 }
 
-function getCityImageURL(cityIdURL, searchCity) {
+function getCityImageURL(cityIdURL, cityNameAPI) {
     var imageURL;
     fetch(cityIdURL).then(function(response) {
         return response.json();
@@ -165,11 +70,11 @@ function getCityImageURL(cityIdURL, searchCity) {
         imageURL = data._links["ua:images"].href;
 
         //console.log(imageURL);
-        getImage(imageURL, searchCity);
+        getImage(imageURL, cityNameAPI);
     })
 }
 
-function getImage(imageURL, searchCity) {
+function getImage(imageURL, cityNameAPI) {
     var headerImage;
     fetch(imageURL).then(function(response) {
         return response.json();
@@ -177,14 +82,14 @@ function getImage(imageURL, searchCity) {
         headerImageURL = data.photos[0].image.web;
 
         //console.log(headerImageURL)
-        setHeaderInfo(headerImageURL, searchCity);
+        setHeaderInfo(headerImageURL, cityNameAPI);
     });
 }
 
-function setHeaderInfo(headerImageURL, searchCity) {
+function setHeaderInfo(headerImageURL, cityNameAPI) {
     cityHeader.removeAttribute("class", "hidden")
     cityHeader.setAttribute("style", `background-image:url("${headerImageURL}")`)
-    cityName.innerHTML = searchCity;
+    cityName.innerHTML = cityNameAPI;
 }
 
 // Hotels
