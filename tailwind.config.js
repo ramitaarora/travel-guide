603392a55ce3571c3526a72111dbf8d8e786c6ea
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-09-23 16:33:04 -0700

    fixed header

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './assets/script.js', "./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
